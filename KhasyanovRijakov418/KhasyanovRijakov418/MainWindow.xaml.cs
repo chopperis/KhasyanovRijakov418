dcba904b24880173064efa81c733a1ae2420f166
Dimabox05@mail.ru <Dimabox05@mail.ru>
2021-10-21 00:04:06 +0300

    PR4 Complete

--- KhasyanovRijakov418/KhasyanovRijakov418/MainWindow.xaml.cs
+++ KhasyanovRijakov418/KhasyanovRijakov418/MainWindow.xaml.cs
@@ -71,17 +71,74 @@ namespace KhasyanovRijakov418
 
             using (var db = new Entities())
             {
-                string IDline = String.Join(
-                    ":", db.Users.Select(x => x.Login),
-                    ":", db.Users.Select(x => x.Password),
-                    ":", db.Users.Select(x => x.Role),
-                    ":", db.Users.Select(x => x.FIO),
+                string IDline;
+
+                IDline = String.Join(
                     ":", db.Users.Select(x => x.ID));
+                IDline = DelP(IDline);
                 sw.WriteLine(IDline);
+
+                IDline = String.Join(
+                    ":", db.Users.Select(x => x.FIO));
+                IDline = DelP(IDline);
+                sw.WriteLine(IDline);
+
+                IDline = String.Join(
+                    ":", db.Users.Select(x => x.Login));
+                IDline = DelP(IDline);
+                sw.WriteLine(IDline);
+
+                IDline = String.Join(
+                    ":", db.Users.Select(x => x.Password));
+                IDline = DelP(IDline);
+                sw.WriteLine(IDline);
+
+                IDline = String.Join(
+                    ":", db.Users.Select(x => x.Role));
+                IDline = DelP(IDline);
+                sw.WriteLine(IDline);
+
                 sw.Close();
 
                 Process.Start("notepad.exe", path);
             }
+        }
+
+        private void Import(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.FileName != "") // проверка на выбор файла
+            {
+                StreamReader sr = new StreamReader(ofd.FileName); //открываем файл
+                while (!sr.EndOfStream) // перебираем строки, пока они не
+                {
+                    string[] lines = new string[5];// массив данных
+                    for (int i = 0; i < 5; i++) // читаем 5 строк
+                    {
+                        string line = sr.ReadLine(); // читаем строку
+                        string[] data = line.Split(':');
+                        line = ""; // обнуляем переменную
+                        if (data.Length >= 2) // проверяем на целостностьданных
+                        {
+                            for (int j = 1; j < data.Length; j++) // складываем данные
+                            {
+                                line += data[j]; // собираем строку
+                            }
+                        }
+                        lines[i] = line; // записываем значения в массив
+                    }
+                    // выводим данные
+                    MessageBox.Show("Данные в файле: \nID: " + lines[0] +
+                    "\nФИО: " + lines[1] + "\nЛогин: " + lines[2] + "\nПароль: " +
+                    lines[3] + "\nРоль: " + lines[4]);
+                }
+            }
+            else MessageBox.Show("Файл для импорта не выбран!");
+        }
+
+        public string DelP(string text) {
+            return text = text.Trim(new char[] { ' ' });
         }
     }
 }
